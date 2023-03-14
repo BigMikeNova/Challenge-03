786386cdda6efa4f2dd3dc626b25ef3227ea74c3
BigMikeNova <125073452+BigMikeNova@users.noreply.github.com>
2023-03-13 20:36:21 -0400

    Camel Cased javascript

--- Develop/script.js
+++ Develop/script.js
@@ -1,18 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var Length;
-var Lower;
-var Upper;
-var Special;
-var Choices;
+var length;
+var lower;
+var upper;
+var special;
+var choices;
 
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var blankUpper = [];
-var toUpper = function (x) {
-  return x.toUpperCase();
+var blankupper = [];
+var toupper = function (x) {
+  return x.toupperCase();
 };
-upperCase = lowerCase.map(toUpper);
+upperCase = lowerCase.map(toupper);
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var special = ["!", "#", "$", "%", "&", "*", "/", " < ", " > ", " ? ", "@", "_", "~"];
 
@@ -29,92 +29,92 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
-  Length = prompt("How many characters would you like your password? Choose between 8 and 128");
-  console.log("Password length " + Length);
-  if(!Length) {
+  length = prompt("How many characters would you like your password? Choose between 8 and 128");
+  console.log("Password length " + length);
+  if(!length) {
     alert("Required value");
-  } else if (Length < 8 || Length > 128) {
-    Length = prompt("You must choose between 8 and 128");
-    console.log("Password length " + Length);  
+  } else if (length < 8 || length > 128) {
+    length = prompt("You must choose between 8 and 128");
+    console.log("Password length " + length);  
   } else { 
-    Lower = confirm("Need lower case letters?");
-    console.log("Lower case " + Lower);
-    Upper = confirm("Need upper case letters?");
-    console.log("Upper case " + Upper);
+    lower = confirm("Need lower case letters?");
+    console.log("lower case " + lower);
+    upper = confirm("Need upper case letters?");
+    console.log("upper case " + upper);
     Number = confirm("Need numbers?");
     console.log("Number " + Number);
-    Special = confirm("Need special characters?");
-    console.log("Special Character " + Special);
+    special = confirm("Need special characters?");
+    console.log("special Character " + special);
   };
-  if (!Lower && !Upper && !Number && !Special) {
-    Choices = alert("Gotta pick something :)");
-  } else if (Lower && Upper && Number && Special) {
-    Choices = lowerCase.concat(upperCase, numbers, special);
-    console.log(Choices);
+  if (!lower && !upper && !Number && !special) {
+    choices = alert("Gotta pick something :)");
+  } else if (lower && upper && Number && special) {
+    choices = lowerCase.concat(upperCase, numbers, special);
+    console.log(choices);
   }
-  else if (Lower && Upper && Number) {
-    Choices = lowerCase.concat(upperCase, numbers);
-    console.log(Choices);
+  else if (lower && upper && Number) {
+    choices = lowerCase.concat(upperCase, numbers);
+    console.log(choices);
   }
-  else if (Lower && Upper && Special) {
-    Choices = lowerCase.concat(upperCase, special);
-    console.log(Choices);
+  else if (lower && upper && special) {
+    choices = lowerCase.concat(upperCase, special);
+    console.log(choices);
   }
-  else if (Lower && Number && Special) {
-    Choices = lowerCase.concat(numbers, special);
-    console.log(Choices);
+  else if (lower && Number && special) {
+    choices = lowerCase.concat(numbers, special);
+    console.log(choices);
   }
-  else if (Upper && Number && Special) {
-    Choices = upperCase.concat(numbers, special);
-    console.log(Choices);
+  else if (upper && Number && special) {
+    choices = upperCase.concat(numbers, special);
+    console.log(choices);
   }
-  else if (Lower && Upper) {
-    Choices = lowerCase.concat(upperCase);
-    console.log(Choices);
+  else if (lower && upper) {
+    choices = lowerCase.concat(upperCase);
+    console.log(choices);
   }
-  else if (Lower && Number) {
-    Choices = lowerCase.concat(numbers);
-    console.log(Choices);
+  else if (lower && Number) {
+    choices = lowerCase.concat(numbers);
+    console.log(choices);
   }
-  else if (Lower && Special) {
-    Choices = lowerCase.concat(special);
-    console.log(Choices);
+  else if (lower && special) {
+    choices = lowerCase.concat(special);
+    console.log(choices);
   }
-  else if (Upper && Number) {
-    Choices = upperCase.concat(numbers);
-    console.log(Choices);
+  else if (upper && Number) {
+    choices = upperCase.concat(numbers);
+    console.log(choices);
   }
-  else if (Upper && Special) {
-    Choices = upperCase.concat(special);
-    console.log(Choices);
+  else if (upper && special) {
+    choices = upperCase.concat(special);
+    console.log(choices);
   }
-  else if (Number && Special) {
-    Choices = numbers.concat(special);
-    console.log(Choices);
+  else if (Number && special) {
+    choices = numbers.concat(special);
+    console.log(choices);
   }
-  else if (Lower) {
-    Choices = lowerCase;
-    console.log(Choices);
+  else if (lower) {
+    choices = lowerCase;
+    console.log(choices);
   }
-  else if (Upper) {
-    Choices = blankUpper.concat(upperCase);
-    console.log(Choices);
+  else if (upper) {
+    choices = blankupper.concat(upperCase);
+    console.log(choices);
   }
   else if (Number) {
-    Choices = numbers;
-    console.log(Choices);
+    choices = numbers;
+    console.log(choices);
   }
-  else if (Special) {
-    Choices = special;
-    console.log(Choices);
+  else if (special) {
+    choices = special;
+    console.log(choices);
   };
 
   var passwordBlank = [];
   
-  for (var i = 0; i < Length; i++) {
-    var allChoices = Choices[Math.floor(Math.random() * Choices.length)];
-    passwordBlank.push(allChoices);
-    console.log(allChoices);
+  for (var i = 0; i < length; i++) {
+    var allchoices = choices[Math.floor(Math.random() * choices.length)];
+    passwordBlank.push(allchoices);
+    console.log(allchoices);
   }
 
   var password = passwordBlank.join("");
