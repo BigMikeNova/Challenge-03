89f9226dcbc172ed6ae9230a2ca7eb8ead3345f9
BigMikeNova <125073452+BigMikeNova@users.noreply.github.com>
2023-03-23 18:43:51 -0400

    fixed glitch with toUpperCase() function. now working.

--- Develop/script.js
+++ Develop/script.js
@@ -9,10 +9,10 @@ var choices;
 
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var blankupper = [];
-var toupper = function (x) {
-  return x.toupperCase();
+var toUpper = function (x) {
+  return x.toUpperCase();
 };
-upperCase = lowerCase.map(toupper);
+upperCase = lowerCase.map(toUpper);
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var special = ["!", "#", "$", "%", "&", "*", "/", " < ", " > ", " ? ", "@", "_", "~"];
 
